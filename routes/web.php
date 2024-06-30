2a603d8391ded7774ad8405ddea5e4de9cd06ea8
reskiridani <reskiridani@gmail.com>
2024-06-30 21:55:03 +0700

    Modul1

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,19 @@ Route::get('/dashboard', [App\Http\Controllers\DashboardController::class,
             'index'])->name('dashboard');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
